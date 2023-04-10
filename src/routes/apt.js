5df90bc1a85233486046c0c88820fd6edcadb0e9
Jon <68436526+jwil32@users.noreply.github.com>
2023-04-10 12:52:02 -0600

    code for adding sources

--- src/routes/apt.js
+++ src/routes/apt.js
@@ -3,15 +3,37 @@ const router = express.Router();
 const apt = require('../services/apt');
 const auth = require('../services/auth');
 
-// was router.create
-// needs authentication
 router.post('/', auth.isAuthenticated, async function (req, res) {
     try {
-        res.json(await apt.createOne(req.body.aptNation, req.body.aptDesc, req.body.aptFirstSeen, req.body.aptName, req.session.user));
+        result = await apt.createOne(req.body.aptNation, req.body.aptDesc, req.body.aptFirstSeen, req.body.aptName, req.session.user, req.body);
     } catch (err) {
         console.error(`Error could not create APT:`, err.message);
         res.status(500).send(err);
     }
+    if(parseInt(req.body.numSources) > 0){
+        try {
+            id = await apt.getIDbyName(req.body.aptName);
+            id = id[0].APTID;
+            console.log(id);
+        } catch {
+            console.error(`Error could find APT:`, err.message);
+            res.status(500).send(err);
+        }
+
+        for(i=1;i < parseInt(req.body.numSources) + 1; i++){
+            objName = "source" + i;
+            console.log("body source #" + i + "= " + req.body[objName]);
+            if(req.body[objName].length > 0){
+                try {
+                    insert = await apt.addSource(id, req.body[objName]);
+                } catch {
+                    console.error(`Error inserting source:`, err.message);
+                    res.status(500).send(err);
+                }
+            }
+        }
+    }
+    res.json(result);
 });
 
 router.get('/sources/:id', async function (req, res){
